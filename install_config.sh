25d4095d50331dfe54e2487c1569486987414bc0
Reto Eberle <reto.eberle@gmail.com>
2018-01-30 22:44:18 +0100

    folder structure for vim packages (as git submodules) will be created/linked.

--- install_config.sh
+++ install_config.sh
@@ -3,9 +3,11 @@
 #chmod a+x vim_install.sh -> Gives everyone execute permissions
 #sudo ./install_config.sh
 
-echo "link the dotfiles"
+echo "link the dotfiles and the vim packages"
 ln -svi $PWD/.bashrc ~
 ln -svi $PWD/.vimrc ~
+mkdir -p ~/.vim/pack
+ln -svi $PWD/.vim/pack/gitsubm_mnged ~/.vim/pack
 ln -svi $PWD/.gitconfig ~
 ln -svi $PWD/.tmux.conf ~
 
